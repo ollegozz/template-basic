0ff00a2ce7debc435bce548af2e119528fd5bd80
Олег <OllegOzz@yandex.ru>
2023-07-25 23:23:29 +0300

    header-test

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,9 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
+let menuHeader = document.querySelector('.menu__list');
 
 menuBtn.addEventListener('click', function () {
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
+    menuHeader.classList.toggle('_none');
 })
