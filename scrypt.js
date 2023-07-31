9eaf2d2253ae0acf961846fa95c8941551000d1c
Олег <OllegOzz@yandex.ru>
2023-07-31 19:02:09 +0300

    header-menu

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,9 @@
 let iconMenu = document.querySelector('.icon-menu');
+let menuBody = document.querySelector('.menu__body');
 
 iconMenu.addEventListener('click', function () {
     iconMenu.classList.toggle('icon-menu');
     iconMenu.classList.toggle('close-icon-menu');
+    menuBody.classList.toggle('active');
 })
 
