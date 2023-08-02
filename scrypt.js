5583c65c27cd830b0ba9348b2e3e34c83e4cb22f
Олег <OllegOzz@yandex.ru>
2023-08-02 16:22:30 +0300

    header-menu-anim

--- scrypt.js
+++ scrypt.js
@@ -5,24 +5,6 @@ const button = document.querySelector('.header__button');
 
 iconMenu.addEventListener('click', function () {
 
-    // let aaa = 0
-
-    // if (aaa == 0) {
-    //     menuBody.classList.remove('animOpen');
-    //     menuBody.classList.add('animOpen');
-    //     console.log('animOpen');        
-    //     aaa = aaa + 1
-    // } 
-    
-    // if (aaa !== 0) {        
-    //     menuBody.classList.add('animClose');   
-    //     console.log('animanimClose');
-        
-    //     aaa = aaa - 1
-    //     console.log('CLOSE', aaa);       
-        
-    // }
-
    
     button.classList.toggle('active-button');
     iconMenu.classList.toggle('icon-menu');
@@ -32,9 +14,7 @@ iconMenu.addEventListener('click', function () {
     if (closeIconMenu.classList.contains('close-icon-menu')) {
         menuBody.classList.toggle('animClose');
     }
-
-
-        
+    
         
     
     
