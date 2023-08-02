f32763fab26cefbfbf3e6724b394ff29bdb0b90b
Олег <OllegOzz@yandex.ru>
2023-08-02 17:05:42 +0300

    header done

--- scrypt.js
+++ scrypt.js
@@ -1,22 +1,17 @@
-const iconMenu = document.querySelector('.icon-menu');
-const closeIconMenu = document.querySelector('.close-icon-menu');
-const menuBody = document.querySelector('.menu__body');
-const button = document.querySelector('.header__button');
+function showBurger() {
+    const iconMenu = document.querySelector('.icon-menu');
+    const menuBody = document.querySelector('.menu__body');
+    const button = document.querySelector('.header__button');
 
-iconMenu.addEventListener('click', function () {
+    iconMenu.addEventListener('click', function () {
+        button.classList.toggle('active-button');
+        iconMenu.classList.toggle('icon-menu');
+        iconMenu.classList.toggle('close-icon-menu');
+        menuBody.classList.toggle('active');
+        menuBody.classList.toggle('animOpen');
+        // menuBody.classList.toggle('animClose');
 
-   
-    button.classList.toggle('active-button');
-    iconMenu.classList.toggle('icon-menu');
-    iconMenu.classList.toggle('close-icon-menu');
-    menuBody.classList.toggle('active');
+    })  
+}
 
-    if (closeIconMenu.classList.contains('close-icon-menu')) {
-        menuBody.classList.toggle('animClose');
-    }
-    
-        
-    
-    
-    
-})
+showBurger()
