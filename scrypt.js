ab57f3ff8929688ec936d981953e0de9eb321dbc
Олег <OllegOzz@yandex.ru>
2023-08-04 17:34:55 +0300

    carousel

--- scrypt.js
+++ scrypt.js
@@ -11,7 +11,35 @@ function showBurger() {
         menuBody.classList.toggle('animOpen');
         // menuBody.classList.toggle('animClose');
 
-    })  
+    })
 }
 
 showBurger()
+
+const arr = document.querySelector('.carousel-item');
+
+let currentIndex = 0;
+const carouselItems = document.querySelectorAll('.carousel-item');
+function goToSlide(index) {
+    if (index === 0) {
+        index = carouselItems.length - 1;
+    } else if (index === carouselItems.length) {
+        index = 0;
+    }
+    currentIndex = index;
+    document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 34}%)`;
+}
+function goToNextSlide() {
+    goToSlide(currentIndex + 1);
+}
+function goToPrevSlide() {
+    goToSlide(currentIndex - 1);
+}
+setInterval(goToNextSlide, 3000);
+
+
+
+arr.addEventListener('click', function () {
+    goToPrevSlide()
+    console.log(carouselItems);
+})
