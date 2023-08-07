12c247aa452c293969287724a97ad9fc1648bf47
Олег <OllegOzz@yandex.ru>
2023-08-07 14:34:15 +0300

    carousel-arrows

--- scrypt.js
+++ scrypt.js
@@ -16,7 +16,8 @@ function showBurger() {
 
 showBurger()
 
-const arr = document.querySelector('.carousel-item');
+const carouselArrows = document.querySelectorAll('.carousel__arrow');
+
 
 let currentIndex = 0;
 const carouselItems = document.querySelectorAll('.carousel-item');
@@ -27,19 +28,52 @@ function goToSlide(index) {
         index = 0;
     }
     currentIndex = index;
-    document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 34}%)`;
+    document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 function goToNextSlide() {
     goToSlide(currentIndex + 1);
 }
 function goToPrevSlide() {
+    if (currentIndex === 0 ) {
+        currentIndex = carouselItems.length 
+    }
     goToSlide(currentIndex - 1);
 }
-setInterval(goToNextSlide, 3000);
+
+function initArrows() {
+    carouselArrows.forEach(arrow => {
+        arrow.addEventListener("click", function () {
+            if (arrow.dataset.dir === 'left') {
+                goToPrevSlide()
+                console.log(currentIndex);
+            } else {
+                goToNextSlide()
+                console.log(currentIndex);
+            }
+        })
+    })
+}
+
+// console.log(carouselArrows);
+
+initArrows()
+
+
+// arr.forEach((item) => {
+//     if (item.dataset.dir === 'left') {
+//         console.log(item.dataset.dir)
+//         goToNextSlide()
+//     } else {
+//         goToPrevSlide()
+//         console.log(item.dataset.dir)
+//     } 
+// })
 
 
 
-arr.addEventListener('click', function () {
-    goToPrevSlide()
-    console.log(carouselItems);
-})
+// arr.addEventListener('click', function () {
+//     goToNextSlide()
+//     console.log(currentIndex);
+// })
+
+// console.log(arr.dataset.dir);
