c256d8a44339ece1d969a004f2ecb780424402f0
Олег <OllegOzz@yandex.ru>
2023-08-09 13:21:09 +0300

    ref

--- scrypt.js
+++ scrypt.js
@@ -10,47 +10,50 @@ function showBurger() {
         menuBody.classList.toggle('active');
         menuBody.classList.toggle('animOpen');
         // menuBody.classList.toggle('animClose');
-
     })
 }
 
 showBurger()
 
-const carouselArrows = document.querySelectorAll('.carousel__arrow');
-const carouselItems = document.querySelectorAll('.carousel-item');
+function carousel() {
+    const carouselArrows = document.querySelectorAll('.carousel__arrow');
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    let currentIndex = 0;
 
-let currentIndex = 0;
-
-function goToSlide(index) {
-    if (index === 0) {
-        index = carouselItems.length - 1;
-    } else if (index === carouselItems.length) {
-        index = 0;
+    function goToSlide(index) {
+        if (index === 0) {
+            index = carouselItems.length - 1;
+        } else if (index === carouselItems.length) {
+            index = 0;
+        }
+        currentIndex = index;
+        document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 100}%)`;
     }
-    currentIndex = index;
-    document.querySelector('.carousel-inner').style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-function goToNextSlide() {
-    goToSlide(currentIndex + 1);
-}
-function goToPrevSlide() {
-    if (currentIndex === 0 ) {
-        currentIndex = carouselItems.length
-    } 
-    goToSlide(currentIndex - 1);
-}
+    function goToNextSlide() {
+        goToSlide(currentIndex + 1);
+    }
+    function goToPrevSlide() {
+        if (currentIndex === 0) {
+            return false
+        }
+        goToSlide(currentIndex - 1);
+    }
 
-function initArrows() {
-    carouselArrows.forEach(arrow => {
-        arrow.addEventListener("click", function () {
-            if (arrow.dataset.dir === 'left') {
-                goToPrevSlide()
-                console.log(currentIndex);
-            } else {
-                goToNextSlide()
-            }
+    function initArrows() {
+        carouselArrows.forEach(arrow => {
+            arrow.addEventListener("click", function () {
+                if (arrow.dataset.dir === 'left') {
+                    goToPrevSlide()
+                    console.log(currentIndex);
+                } else if (arrow.dataset.dir === 'right') {
+                    goToNextSlide()
+                    console.log('R',currentIndex);
+                }
+            })
         })
-    })
+    }
+
+    initArrows()
 }
 
-initArrows()
+carousel()
